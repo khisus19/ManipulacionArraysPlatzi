653ebf3e1f93e51fd81e81f70d226e0d2a390620
Jesús Álvarez <khisus19@gmail.com>
2022-06-03 23:17:33 -0500

    .map() in array of objects

--- server/02-map.js
+++ server/02-map.js
@@ -17,19 +17,65 @@ let arr1 = [1, 2, 3, 4];
 arr1.push(...arr1.map((element) => {
     return element * element;
 }))
-console.log(arr1);
+// console.log(arr1);
 
 let arr2 = [1, 2, 3, 4];
 arr2.forEach((element, index) => {
     arr2[index] = element * element;
 })
-console.log(arr1);
+// console.log(arr1);
 
 // Function that return every element multiply by 2
 function solution(array) {
-    return array.map((element) => {
-        return element * 2;
-    })
+    console.log(array.map((element) => element * 2))
 }
 
-console.log(solution([2, 4, 5, 6]))
+solution([2, 4, 5, 6])
+
+// ---- OTRO EJEMPLO ---- ARRAY DE OBJETOS
+const orders = [
+    {
+      customerName: "Nicolas",
+      total: 60,
+      delivered: true,
+    },
+    {
+      customerName: "Zulema",
+      total: 120,
+      delivered: false,
+    },
+    {
+      customerName: "Santiago",
+      total: 180,
+      delivered: true,
+    },
+    {
+      customerName: "Valentina",
+      total: 240,
+      delivered: true,
+    },
+  ];
+
+/* function names(array) {
+    return array.map((element) => {
+        element.tax = .19;
+        return element;
+    })
+} */
+// CUIDADO de esta forma mutamos el objeto original
+// const result = names(orders);
+// console.log(result);
+// console.log(orders);
+
+function names2(array) {
+    return array.map((element) => {
+        return {
+            ...element,
+            tax: .19,
+        }; // De esta forma nos aseguramos de no modificar el objeto original
+    });
+}
+
+const result2 = names2(orders);
+console.log(result2);
+console.log(orders);
