6aec3210fea4191d030fe0128982c841c5296381
Jesús Álvarez <khisus19@gmail.com>
2022-06-07 12:49:38 -0500

    .flat method

---
+++ server/14-flat.js
@@ -0,0 +1,20 @@
+// .flat() method
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6, [10, 11]],
+    [7, 8, 9]
+];
+
+// flat with for by only two levels deep
+const newArray = [];
+for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+        newArray.push(matrix[i][j])
+    }
+}
+console.log(newArray);
+
+// .flat() method
+let newArray1 = matrix.flat(2);
+console.log(newArray1);
